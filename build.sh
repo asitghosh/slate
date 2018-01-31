48ad10ea75bdac4abbd82fa233120d32bc3c306c
Adrien Zieba <adrien.zieba@appdirect.com>
2018-01-31 11:15:57 -0500

    API-172 : Change search page to be processed by middleman during build Replace url in searchPage and layout with snippet which gets information in configuration file Modify config to exclude layout for searchPage and add url for each environment change extract script to build dev and prod documentation Add tag test for build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t docker.appdirect.tools/documentation/slate .
+docker build -t docker.appdirect.tools/documentation/slate:test .
--- slate-extract.sh
+++ slate-extract.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
-# This script extract sources from docker image slate
+# This script extract sources for DEV and PROD from docker image slate
 # Mainly used to validate docker image is ok
 # Pass version you want run in argument
 set -e
 
-SLATE_VERSION=latest
+SLATE_VERSION=unknown
 if [ ! -z "$1" ];
 then
 	SLATE_VERSION=$1
 fi
 
-echo "Remove docker container named 'slate-extract' if present"
-docker rm --force slate-extract || true
-echo "Run slate:$SLATE_VERSION to extract sources"
-docker run --env goal_env=build --name slate-extract docker.appdirect.tools/documentation/slate:${SLATE_VERSION}
-docker wait slate-extract
-echo "Copy source to ./build folder"
-docker cp slate-extract:/app/slate/build ./build
+function slate_build {
+	echo "Remove docker container named 'slate-extract' if present"
+	docker rm --force slate-extract || true
+	echo "Run slate:$SLATE_VERSION with MIDDLEMAN_COMMAND=$1 to extract sources"
+	docker run --env MIDDLEMAN_COMMAND="$1" --name slate-extract docker.appdirect.tools/documentation/slate:${SLATE_VERSION}
+	docker wait slate-extract
+	echo "Copy source to ./build/$2 folder"
+	docker cp slate-extract:/app/slate/build ./build/$2
+}
+
+echo "Clean Build Folder"
+rm -rf build
+mkdir build
+
+echo "Build slate for DEV"
+slate_build "build -e development" dev
+echo "Build slate for PROD"
+slate_build "build" prod
